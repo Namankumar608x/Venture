a96c29afb1c91658d90ac319f616d3e1ae63f9e3
avadhesh11 <202452309@iiitvadodara.ac.in>
2025-12-05 20:29:36 +0530

    auth

--- Backend/src/db/mongo.js
+++ Backend/src/db/mongo.js
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mongo = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("MongoDB connected");
-//   } catch (err) {
-//     console.error("MongoDB connection error:", err);
-//   }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
 };
 
 export default mongo;
