c03878ede53c59911a8e3127cc599a2187de89ca
Namankumar608x <nmukati3000@gmail.com>
2025-12-26 21:36:40 +0530

    .

--- Backend/src/utils/sendMail.js
+++ Backend/src/utils/sendMail.js
@@ -1,6 +1,9 @@
 import nodemailer from "nodemailer";
 
 export const sendMail = async (to, otp) => {
+  console.log("EMAIL_USER:", process.env.EMAIL_USER);
+  console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
